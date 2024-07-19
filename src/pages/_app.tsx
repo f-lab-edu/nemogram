e2ef383375f790aa0d893221b39a346778c4417d
Sage <prestoferoce@gmail.com>
2024-07-19 11:35:35 +0900

    feat: Layout 및 CssBaseline 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import Layout from "@/components/Layout";
+import { CssBaseline } from "@mui/material";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
