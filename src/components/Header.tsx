b41105c747d783cf73f4e07f43a51b401f79e46b
Sage <prestoferoce@gmail.com>
2024-07-19 11:34:52 +0900

    feat: Header 컴포넌트 마크업

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Nemograms</h1>
+      <div>
+        <button type="button">랜덤문제</button>
+        <button type="button">문제생성</button>
+      </div>
+    </header>
+  );
+}
