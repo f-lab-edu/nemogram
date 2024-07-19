b294668bff3ce894bcdd56691d118af18d7eb6b7
Sage <prestoferoce@gmail.com>
2024-07-19 11:28:59 +0900

    chore: HTML head 메타 태그 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>Nemograms</title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="네모그램" />
+      </Head>
       <body>
         <Main />
         <NextScript />
