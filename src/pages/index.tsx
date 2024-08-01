2e1ff4d7152fbd02c61b8421fca20c10217af318
Sage <prestoferoce@gmail.com>
2024-08-01 17:23:44 +0900

    feat: index 페이지에 임시 퍼즐 리스트 데이터 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,43 @@
 import Head from "next/head";
+import { PuzzleCardListData } from "@/types/puzzleCardTypes";
+
+const puzzleCardListData: PuzzleCardListData = [
+  {
+    id: "1",
+    title: "Sunset",
+    size: [10, 10],
+    image: "#",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Mountain",
+    size: [15, 15],
+    image: "#",
+    completed: true,
+  },
+  {
+    id: "3",
+    title: "Cat",
+    size: [20, 20],
+    image: "#",
+    completed: false,
+  },
+  {
+    id: "4",
+    title: "Tree",
+    size: [5, 5],
+    image: "#",
+    completed: true,
+  },
+  {
+    id: "5",
+    title: "House",
+    size: [10, 10],
+    image: "#",
+    completed: false,
+  },
+];
 
 export default function Home() {
   return (
