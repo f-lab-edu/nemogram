6641cb98f131298c232f4a4d2d24b23f8c8af945
Sage <prestoferoce@gmail.com>
2024-08-13 18:28:13 +0900

    feat: JSON 파일 불러오도록 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,29 @@
 import Head from "next/head";
-import { PuzzleCardListData } from "@/types/puzzleCardTypes";
+import { promises as fs } from "fs";
+import path from "path";
 import PuzzleCardList from "@/components/PuzzleCardList";
+import { Puzzle } from "@/types/puzzle";
 
-const puzzleCardListData: PuzzleCardListData = [
-  {
-    id: "1",
-    title: "Sunset",
-    size: [10, 10],
-    image: "#",
-    completed: false,
-  },
-  {
-    id: "2",
-    title: "Mountain",
-    size: [15, 15],
-    image: "#",
-    completed: true,
-  },
-  {
-    id: "3",
-    title: "Cat",
-    size: [20, 20],
-    image: "#",
-    completed: false,
-  },
-  {
-    id: "4",
-    title: "Tree",
-    size: [5, 5],
-    image: "#",
-    completed: true,
-  },
-  {
-    id: "5",
-    title: "House",
-    size: [10, 10],
-    image: "#",
-    completed: false,
-  },
-];
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "src/data/mock.json");
+  const fileContents = await fs.readFile(filePath, "utf8");
+  const data: Puzzle[] = JSON.parse(fileContents);
 
-export default function Home() {
+  return {
+    props: {
+      puzzles: data,
+    },
+  };
+}
+
+export default function Home({ puzzles }: { readonly puzzles: Puzzle[] }) {
   return (
     <>
       <Head>
         <title>Nemograms</title>
       </Head>
       <div>
-        <PuzzleCardList listData={puzzleCardListData} />
+        <PuzzleCardList puzzles={puzzles} />
       </div>
     </>
   );
